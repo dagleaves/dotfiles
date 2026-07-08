b37987380237156ba49cc16976581f29a09b1530
Daniel Gleaves <68761152+dagleaves@users.noreply.github.com>
2026-07-08 10:07:29 -0400

    Increase wezterm opacity

--- home/wezterm.lua
+++ home/wezterm.lua
@@ -5,12 +5,12 @@ local config = wezterm.config_builder()
 local is_windows = os.getenv("OS") and os.getenv("OS"):lower():find("windows")
 local is_macos = wezterm.target_triple:lower():find("darwin") ~= nil
 
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.9
 config.color_scheme = "rose-pine-moon"
 config.max_fps = 120
 config.enable_tab_bar = false
 config.font = wezterm.font("Hack Nerd Font", { weight = "DemiBold" })
-config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.window_decorations = "RESIZE"
 config.window_frame = {
   font = wezterm.font("Hack Nerd Font", { weight = "Bold" }),
 }
